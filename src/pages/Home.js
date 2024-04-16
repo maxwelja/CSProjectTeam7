369e22bbf72eaf27d0311a33a76104a1111e1fa6
sthitakham <151214547+sthitakham@users.noreply.github.com>
2024-04-16 13:49:40 -0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,59 @@
+import "bootstrap/dist/css/bootstrap.css";
 import './Home.css';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import { Button, Container, Col, Row, Image} from 'react-bootstrap';
-import DropdownMenu from '../components/DropdownMenu';
-import NavBar from '../components/NavBar';
+import DropdownMenu from './DropdownMenu';
 
-function Home(){
-    return(
-        <div className='Home'>
-            <NavBar />
-        </div>
-    )
 
+function Home() {
+  return (
+    <div className="volunteerProfile">
+      <Row id="main">
+
+          <Col id="mainCol">
+            <Container id="container1">
+              <Col>
+                <DropdownMenu />
+              </Col>
+              <Col id="logo">
+
+              </Col>
+              <Col id="title" xs={6} md={4}>
+                Welcome!
+              </Col>
+              <Col xs={6} md={4}>
+                <Container id="logo2">
+                  <Image src="images/logo100.png" />
+                </Container>
+              </Col>
+            </Container>
+
+            <Row id="row2">
+              <Col>
+                < Container id="container2">
+                {/* ProfilePic | VolunteerInfo */}
+                  <Col id="infoColumn">
+                    <Image src="images/logo_place.png" />
+                  </Col>
+                </Container>
+              </Col>
+            </Row>
+            <div className='box' id='box1'>
+              <Container id="container3">
+                <Button variant='primary' size="lg" id="btn1">Profile</Button>
+                <Button variant='primary' size="lg" id="btn2">Events Calendar</Button>
+                <Button variant='primary' size="lg" id="btn3"> Event List</Button>
+                <Button variant='primary' size="lg" id="btn4">Volunteer Hours</Button>
+              </Container>
+              <Col id="bot-right">
+              </Col>
+            </div>
+          </Col>
+
+      </Row>
+    </div>
+  );
 }
 
 export default Home;
