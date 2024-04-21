9e981115ccde07c09e9335533e31f707ba759ca6
Jacob Maxwell <37170978+maxwelja@users.noreply.github.com>
2024-04-21 14:24:07 -0500

    Add files via upload

--- server/routes/events.js
+++ server/routes/events.js
@@ -1,12 +1,27 @@
-const express = require('express')
-const router = express.Router()
-const {createEvent, readEvent, readAll, updateEvent, deleteEvent} = require('../controllers/eventManager')
+const express = require('express');
+const router = express.Router();
+const {createEvent, readEvent, readAll, updateEvent, deleteEvent} = require('../controllers/eventManager');
 
 // CRUD operations for events
-router.post('/', createEvent)
-router.get('/:id', readEvent)
-router.get('/', readAll)
-router.patch('/:id', updateEvent)
-router.delete('/:id', deleteEvent)
+router.get('/all', (req, res) => {
+    res.json({mssg:"Get all events"});
+})
+router.post('/tmp', async (req,res) =>{
+    const {title, organization, location} = req.body;
 
-module.exports = router
+    // try to create a new event
+    try {
+        const event = await Event.create({title, organization, location});
+        res.status(200).json(event);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+    res.json({msg: 'Post an event'});
+})
+router.post('/', createEvent);
+router.get('/:id', readEvent);
+router.get('/', readAll);
+router.patch('/:id', updateEvent);
+router.delete('/:id', deleteEvent);
+
+module.exports = router;
